56b298063ab9455dd2f10d5829e62cc3284353e3
oliveiramatheux <matheus.oliveira0730@gmail.com>
2023-06-15 15:42:24 -0300

    fix datepicker position on form and terms text

--- src/modules/components/TermsAndConditions/index.tsx
+++ src/modules/components/TermsAndConditions/index.tsx
@@ -18,17 +18,17 @@ const TermsAndConditions = (props: InterfaceTermsAndConditionsProps): JSX.Elemen
         description={
           <div className={classes.divContentModalScroll}>
             <p className={classes.textModalScroll}>1. Escambooks é uma rede social voltada para a interação entre
-            os usuários e a troca de livros.</p>
+            os usuários e a troca de livros;</p>
             <p className={classes.textModalScroll}>2. Os dados dos usuários cadastrados são utilizados por e somente pela plataforma
             Escambooks, a fim de disponibilizar uma melhor usabilidade e experiência. Não disponibilizamos dados de usuários para terceiros, campanhas
-            de marketing, anúncios ou mídia social.</p>
+            de marketing, anúncios ou mídia social;</p>
             <p className={classes.textModalScroll}>3. Todos os direitos da plataforma são reservados ao Escambooks, qualquer plágio
-            será tomado as devidas diretrizes administrativas.</p>
+            será tomado as devidas diretrizes administrativas;</p>
             <p className={classes.textModalScroll}>4. Após o match na plataforma, não nos responsabilizamos por entregas ou recebimentos
-            de livros trocados pelos usuários da plataforma, cabe aos mesmos se responsabilizar.</p>
+            de livros trocados pelos usuários da plataforma, cabe aos mesmos se responsabilizar;</p>
             <p className={classes.textModalScroll}>5. Não nos responsabilizamos pelo uso indevido dos livros publicados pelos
-            pequenos autores.</p>
-            <p className={classes.textModalScroll}>6. As Atualizações sobre os livros para vestibulares acontencerão a cada 6 meses para
+            pequenos autores;</p>
+            <p className={classes.textModalScroll}>6. As Atualizações sobre os livros para vestibulares acontencerão a cada seis meses para
             as universidades contempladas.</p>
           </div>
         }
